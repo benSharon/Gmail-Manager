fe860ffdac776b08ad4ac0f1141c3e0a76165ed7
benSharon <55289799+benSharon@users.noreply.github.com>
2024-08-10 23:15:33 +0300

    Update flow.py
    Added decorator for each flow

--- flow.py
+++ flow.py
@@ -1,7 +1,27 @@
 import time
-from handler import *
+import os
+from handler import (create_message,
+                     send_message,
+                     get_message_ids,
+                     delete_messages)
 
 
+def clean_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def section_header(flow_task):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            clean_screen()
+            print(f'{flow_task}:')
+            print('=' * len(flow_task))
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@section_header('SEND_MESSAGE:')
 def send_message_flow(service_api):
     sender = input('Enter sender email: ')
     destination = input('Enter destination email: ')
@@ -15,25 +35,32 @@ def send_message_flow(service_api):
     return
 
 
+@section_header('DELETE_MESSAGE:')
 def delete_message_flow(service_api):
     query = input('Enter query (ex: \'from: <email/name>\' or \'subject: <subject string>\') \n'
                   'or type \'back\' to go back to main menu: ')
+
     while query != 'back':
         messages = get_message_ids(service_api, query=query)
         print(messages)
         print(f'Number of messages: {len(messages)}')
+
         if len(messages) == 0:
             print('No message-ids have been retrieved.')
             input('Press \'Enter\' to get back to the main menu...')
             return
         # batchDelete maximum is 1000 message-ids
+
         if len(messages) > 1000:
             messages = messages[:1000]
             print('Reducing number of message-ids to 1000')
         delete_choice = input('Are you sure you want to delete (yes or no): ')
+
         if delete_choice == 'no':
+            print("Deletion aborted...")
             input('Press \'Enter\' to get back to the main menu...')
             return
+
         if delete_choice == 'yes':
             print('Deleting messages...')
             delete_messages(service_api, messages)
@@ -41,12 +68,14 @@ def delete_message_flow(service_api):
             print('Messages deleted.')
             input('Press \'Enter\' to get back to the main menu...')
             return
+
         else:
             print('Aborted message delete.')
             input('Press \'Enter\' to get back to the main menu...')
             return
 
 
+@section_header('GET_MESSAGE_IDs:')
 def get_message_ids_flow(service_api):
     query = input('Enter query (ex: \'from: <email/name>\' or \'subject: <subject string>\') \n'
                   'or type \'back\' to go back to main menu: ')
@@ -56,6 +85,7 @@ def get_message_ids_flow(service_api):
             print('No message-ids have been retrieved.')
             input('Press \'Enter\' to get back to the main menu...')
             return
+
         else:
             print(messages)
             print(f'Number of messages: {len(messages)}')
