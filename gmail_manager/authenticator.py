7b69e788de2252a363f8edabd89ada3f5ca0dadf
benSharon <55289799+benSharon@users.noreply.github.com>
2024-08-12 01:47:18 +0300

    Grouped files
    Grouped files into gmail_manager directory

---
+++ gmail_manager/authenticator.py
@@ -0,0 +1,39 @@
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+# If modifying these scopes, delete the file token.json.
+# "https://www.googleapis.com/auth/gmail.modify" DOES NOT give you permission to delete.
+# "https://mail.google.com" gives full access to the account - which includes delete permission.
+SCOPES = ["https://mail.google.com/"]
+
+
+def get_gmail_api(creds):
+    service = build("gmail", "v1", credentials=creds)
+    return service
+
+
+def authenticate():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    return get_gmail_api(creds)
