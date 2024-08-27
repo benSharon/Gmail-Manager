cae48e1821f384123fc22827645fd297cf68aee0
benSharon <55289799+benSharon@users.noreply.github.com>
2024-08-27 21:20:27 +0300

    Update authenticator.py using ruff
    used ruff to format the code layout and overall look

--- gmail_manager/authenticator.py
+++ gmail_manager/authenticator.py
@@ -28,9 +28,7 @@ def authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open("token.json", "w") as token:
